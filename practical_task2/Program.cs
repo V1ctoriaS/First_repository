6a3ccac3ab4a09f4eb2b771fcb213d39635aea90
Victoria <si4yova2010@yandex.ru>
2022-12-21 19:14:36 +0500

    task number two of the seminar 1

---
+++ practical_task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.WriteLine("Введите число от 1 до 7");
+
+int number = int.Parse(Console.ReadLine()!);
+
+if (number == 1)
+{
+    Concole.WriteLine("Monday");
+}    
+else if (number == 2)
+{
+    Console.WriteLine("Tuesday");
+}    
+else if (number == 3)
+{
+    Console.WriteLine("Wednesday");
+}    
+else if (number == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if (number == 5)
+{
+    Console.WriteLine("Friday");
+}    
+else if (number == 6)
+{
+    Console.WriteLine("Saturday");
+}    
+else if (number == 7)
+{
+    Console.WriteLine("Sunday");
+}
+else 
+{
+    Console.WriteLine("Введите другое число");
+}
