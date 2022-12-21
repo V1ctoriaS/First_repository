7282cf7b27ff6428212724e5ee2894a8c62dc626
Victoria <si4yova2010@yandex.ru>
2022-12-21 19:14:11 +0500

    task number one of the seminar 1

---
+++ practical_task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число 
+//квадратом второго
+Console.Clear();
+Console.Write("Введите первое число");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Yes");   
+} 
+else
+{
+    Console.WriteLine("No");
+}
